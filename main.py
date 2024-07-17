ab23f6f2c5ff7e40df45f8fecf5a894ac1cbc28d
Raevyn <106601027+rae-vyn@users.noreply.github.com>
2024-07-17 07:46:34 -0400

    clarified the code

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import json
 def main():
     print("### FVF DECK CONVERTER ###")
     print("Uses your player.log file to snag your decks and turns them into deck sharing links.")
+    
+    # Get the player.log path
     print("Enter your player.log file location: ")
     log_location = input()
+
     print()
     found_user_string = ""
+
+    # Search for the player info JSON. Wish this was in its own file, but whatever.
     for line in open(log_location).readlines():
         if line.startswith('===> {"code":0,"user"'):
             found_user_string = line
@@ -14,17 +19,18 @@ def main():
     
     found_user_string = found_user_string[4:].strip()
     playerlog = json.loads(found_user_string)["user"]
+    card_inventory = playerlog["cards"]
+
     for deck in playerlog["decks"]:
-        card_ids = []
+        card_ids = ""
         print(f"Deck #{deck['index'] + 1}: {deck['name']}")
         for card in deck["cards"]:
-            for card_inv in playerlog["cards"]:
-                if card_inv["_id"] == card:
-                    card_ids.append(str(card_inv["cardid"]))
+            for base_card in card_inventory:
+                if base_card["_id"] == card:
+                    card_ids += base_card["cardid"]
+                    
+        print("https://friendsvsfriends.help/?deck=" + card_ids + "\n")
     
-        print("https://friendsvsfriends.help/?deck=" + ".".join(card_ids))
-        print()
-    return 0
 
 
 if __name__ == "__main__":
